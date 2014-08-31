d7cd19fca231de963332cee8327cf6f4f94d13b0
uqw <uqw@outlook.jp>
2014-08-31 19:29:14 +0900

    madd enum testcode

--- jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/javaapi/EnumTest.java
+++ jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/javaapi/EnumTest.java
@@ -1,9 +1,16 @@
 package com.github.sakaguchi3.jbatch002.javaapi;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.EnumMap;
 
 import org.junit.jupiter.api.Test;
+
+import lombok.ToString;
 
 public class EnumTest {
 
@@ -20,6 +27,28 @@ public class EnumTest {
 			var r2 = x.valueOf("xxx");
 		});
 		d();
+	}
+
+	@Test
+	void enumMapTest() {
+		var sut = new EnumSample();
+
+		assertFalse(sut.containsKey(x.j1));
+		assertTrue(sut.containsKey(x.j2));
+		assertEquals("d", sut.get(x.j4));
+		assertNull(sut.get(x.j1));
+	}
+
+	@ToString
+	private static class EnumSample extends EnumMap<x, String> {
+		public EnumSample() {
+			super(x.class);
+			put(x.j2, "b");
+			put(x.j3, "c");
+			put(x.j4, "d");
+		}
+
+		private static final long serialVersionUID = 39192194996601779L;
 	}
 
 	private static enum x {
