c003014c6556f198645e43ab0c1d10cdce3f1dac
uqw <uqw@outlook.jp>
2014-09-16 19:29:14 +0900

    vavr seq

--- jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/vavr/CollectionSeqTest.java
+++ jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/vavr/CollectionSeqTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2021. sakaguchi3, https://github.com/sakaguchi3/learnJava
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.sakaguchi3.jbatch002.vavr;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,19 +24,33 @@ import org.junit.jupiter.api.Test;
 
 import io.vavr.collection.Array;
 import io.vavr.collection.List;
+import io.vavr.collection.Vector;
 
 public class CollectionSeqTest {
 
+//	@Test public void bTest() { d(); }
+
 	@Test
+	public void bTest() {
+		var vec = Vector.of(9, 8, 7, 4, 3, 2);
+
+		var vec1 = vec.sorted();
+		var vec2 = vec.append(3).sorted();
+
+		assertIterableEquals(List.of(9, 8, 7, 4, 3, 2), vec);
+		assertIterableEquals(List.of(2, 3, 4, 7, 8, 9), vec1);
+		assertIterableEquals(List.of(2, 3, 3, 4, 7, 8, 9), vec2);
+	}
+
+//	@Test
 	void lstAppendTest() {
 		// List(1)
 		var lst1 = List.of(1);
 		// List(1, 2)
 		var lst2 = lst1.append(2);
 
-
 		assertIterableEquals(List.of(1, 2), lst2);
-		
+
 		// immutable
 		assertIterableEquals(List.of(1), lst1);
 
