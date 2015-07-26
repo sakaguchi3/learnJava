527f06033fb82c3c3f7d8e956163f52b9e1d2585
uqw <uqw@outlook.jp>
2015-07-26 19:40:52 +0900

    either test

--- jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/vavr/EitherTest.java
+++ jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/vavr/EitherTest.java
@@ -1,6 +1,7 @@
 package com.github.sakaguchi3.jbatch002.vavr;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,19 @@ import io.vavr.control.Either;
 public class EitherTest {
 
 	@Test
-	public void aTest() {
-		debug();
+	void eitherMapLeftTest() {
+		var sut = Either.left(1) //
+				.mapLeft(__ -> 2);
+		assertTrue(sut.isLeft());
+		assertEquals(2, sut.getLeft());
+	}
+
+	@Test
+	void eitherMapRightTest() {
+		var sut = Either.<Integer, String>right("success1") //
+				.map(__ -> "success2");
+		assertTrue(sut.isRight());
+		assertEquals("success2", sut.get());
 	}
 
 	@Test
