8083fd83093065ddb9c77293e2afac1df2bb2e1a
uqw <uqw@outlook.jp>
2015-09-01 14:36:17 +0900

    java mail

--- jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/s/JavaMailTest.java
+++ jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/s/JavaMailTest.java
@@ -1,6 +1,6 @@
 package com.github.sakaguchi3.jbatch002.s;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,39 +10,31 @@ import com.github.sakaguchi3.util.UtilFile;
 public class JavaMailTest {
 
 	@Test
-	public void t01Test() {
+	public void sendMailTest() {
 
-		var reader = new UtilFile();
 		var fileapath = "mail.dat";
-
-		try {
-
-			Optional<String> contentOp = reader.readStrFromResource(fileapath);
-			if (contentOp.isEmpty()) {
-				System.out.println("err");
-				return;
-			}
-
-			JavaMail mail = JavaMail.newInstance();
-
-			String to = "yyy@example.com";
-			String from = "xxx@example.com";
-//			String from = "xxx@example.com";
-//			String senderName = "ushi";
-			String senderName = "_name";
-			String subject = "新年のご挨拶";
-			String content = contentOp.get();
-
-			mail.send(to, from, senderName, subject, content);
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		var contentOp = UtilFile.readStrFromResource(fileapath);
+		if (contentOp.isEmpty()) {
+			System.out.println("err");
+			fail("Not found mail.dat");
+			return;
 		}
 
-		debug();
+		var to = "to@example.com";
+		var from = "from@example.com";
+		var senderName = "_name";
+		var subject = "新年のご挨拶";
+		var content = contentOp.get();
+
+		try { 
+			var mail = JavaMail.newInstance();
+			mail.send(to, from, senderName, subject, content);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
 	}
 
 	private void debug() {
-
 	}
 }
