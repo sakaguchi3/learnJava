b3e2520dc8ecc1b691052dcf90dd3cd6f1012f3d
uqw <uqw@outlook.jp>
2020-09-30 03:34:48 +0900

    for-comprehension

--- jbatch002/src/main/java/com/github/sakaguchi3/util/ForComprehension.java
+++ jbatch002/src/main/java/com/github/sakaguchi3/util/ForComprehension.java
@@ -18,15 +18,19 @@ package com.github.sakaguchi3.util;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import io.vavr.Function3;
+import io.vavr.Function4;
 
 public class ForComprehension {
 
@@ -36,24 +40,22 @@ public class ForComprehension {
 	// - private method
 	// ---------------------------------------------------------------------------------------------
 
-	private static boolean allPresent(Optional<?>... ops) {
+	private static boolean anyMatch(Predicate<Optional<?>> p, Optional<?>... ops) {
 		for (var op : ops) {
-			requireNonNull(op);
-			if (op.isEmpty()) {
-				return false;
+			if (p.test(op)) {
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 
-	private static boolean allPresent(List<?>... ops) {
+	private static boolean anyMatch(Predicate<List<?>> p, List<?>... ops) {
 		for (var op : ops) {
-			requireNonNull(op);
-			if (op.isEmpty()) {
-				return false;
+			if (p.test(op)) {
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 
 	private static void requireNonNulls(Object... arrays) {
@@ -92,6 +94,16 @@ public class ForComprehension {
 		return new For3Optional<>(ts1, ts2, ts3);
 	}
 
+	public static <T1, T2, T3, T4> For4Optional<T1, T2, T3, T4> for_(//
+			Optional<T1> ts1, //
+			Optional<T2> ts2, //
+			Optional<T3> ts3, //
+			Optional<T4> ts4 //
+	) {
+		requireNonNulls(ts1, ts2, ts3, ts4);
+		return new For4Optional<>(ts1, ts2, ts3, ts4);
+	}
+
 	// for list -----------
 
 	public static <T1> For1List<T1> for_(List<T1> ts1) {
@@ -120,7 +132,7 @@ public class ForComprehension {
 	// - class
 	// ---------------------------------------------------------------------------------------------
 
-	// for-optional
+	// optional-for-comprehension
 
 	public static class For1Optional<T1> {
 		private final Optional<T1> ts1;
@@ -163,12 +175,38 @@ public class ForComprehension {
 
 		public <R> Optional<R> yield(Function3<? super T1, ? super T2, ? super T3, ? extends R> f) {
 			requireNonNull(f, "f is null");
-			return ts1.flatMap(t1 -> ts2 //
-					.flatMap(t2 -> ts3.map(t3 -> f.apply(t1, t2, t3))));
+			if (anyMatch(Optional::isEmpty, ts1, ts2, ts3)) {
+				return Optional.empty();
+			}
+			var r = f.apply(ts1.get(), ts2.get(), ts3.get());
+			return Optional.of(r);
 		}
 	}
 
-	// for-list
+	public static class For4Optional<T1, T2, T3, T4> {
+		private final Optional<T1> ts1;
+		private final Optional<T2> ts2;
+		private final Optional<T3> ts3;
+		private final Optional<T4> ts4;
+
+		private For4Optional(Optional<T1> ts1, Optional<T2> ts2, Optional<T3> ts3, Optional<T4> ts4) {
+			this.ts1 = ts1;
+			this.ts2 = ts2;
+			this.ts3 = ts3;
+			this.ts4 = ts4;
+		}
+
+		public <R> Optional<R> yield(Function4<? super T1, ? super T2, ? super T3, ? super T4, ? extends R> f) {
+			requireNonNull(f, "f is null");
+			if (anyMatch(Optional::isEmpty, ts1, ts2, ts3, ts4)) {
+				return Optional.empty();
+			}
+			var r = f.apply(ts1.get(), ts2.get(), ts3.get(), ts4.get());
+			return Optional.of(r);
+		}
+	}
+
+	// list-for-comprehension
 
 	public static class For1List<T1> {
 		private final List<T1> ts1;
@@ -194,9 +232,14 @@ public class ForComprehension {
 
 		public <R> List<R> yield(BiFunction<? super T1, ? super T2, ? extends R> f) {
 			requireNonNull(f, "f is null");
-			return ts1.stream() //
-					.flatMap(t1 -> ts2.stream().map(t2 -> f.apply(t1, t2))) //
-					.collect(toList());
+			if (anyMatch(List::isEmpty, ts1, ts2)) {
+				return Collections.emptyList();
+			}
+			var r = new ArrayList<R>(ts1.size() * ts2.size());
+			for (var t1 : ts1)
+				for (var t2 : ts2)
+					r.add(f.apply(t1, t2));
+			return r;
 		}
 	}
 
@@ -213,11 +256,43 @@ public class ForComprehension {
 
 		public <R> List<R> yield(Function3<? super T1, ? super T2, ? super T3, ? extends R> f) {
 			requireNonNull(f, "f is null");
-			return ts1.stream() //
-					.flatMap(t1 -> ts2.stream() //
-							.flatMap(t2 -> ts3.stream().map(t3 -> f.apply(t1, t2, t3)))) //
-					.collect(toList());
+			if (anyMatch(List::isEmpty, ts1, ts2, ts3)) {
+				return Collections.emptyList();
+			}
+			var r = new ArrayList<R>(ts1.size() * ts2.size() * ts3.size());
+			for (var t1 : ts1)
+				for (var t2 : ts2)
+					for (var t3 : ts3)
+						r.add(f.apply(t1, t2, t3));
+			return r;
 		}
 	}
 
+	public static class For4List<T1, T2, T3, T4> {
+		private final List<T1> ts1;
+		private final List<T2> ts2;
+		private final List<T3> ts3;
+		private final List<T4> ts4;
+
+		private For4List(List<T1> ts1, List<T2> ts2, List<T3> ts3, List<T4> ts4) {
+			this.ts1 = ts1;
+			this.ts2 = ts2;
+			this.ts3 = ts3;
+			this.ts4 = ts4;
+		}
+
+		public <R> List<R> yield(Function4<? super T1, ? super T2, ? super T3, ? super T4, ? extends R> f) {
+			requireNonNull(f, "f is null");
+			if (anyMatch(List::isEmpty, ts1, ts2, ts3, ts4)) {
+				return Collections.emptyList();
+			}
+			var r = new ArrayList<R>(ts1.size() * ts2.size() * ts3.size() * ts4.size());
+			for (var t1 : ts1)
+				for (var t2 : ts2)
+					for (var t3 : ts3)
+						for (var t4 : ts4)
+							r.add(f.apply(t1, t2, t3, t4));
+			return r;
+		}
+	}
 }
