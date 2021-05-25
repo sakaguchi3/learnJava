2ac76bbfa665f1a9b7d21ab20cf8e4ab5b756c94
uqw <uqw@outlook.jp>
2021-05-26 01:45:48 +0900

    null eq test

--- jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/s/AssertTest.java
+++ jbatch002/src/test/java/com/github/sakaguchi3/jbatch002/s/AssertTest.java
@@ -3,21 +3,22 @@ package com.github.sakaguchi3.jbatch002.s;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
-
 
 public class AssertTest {
 
 	@Test
-	public void Test() {
-		debug();
+	public void nullEqualTest() {
+		assertTrue(Objects.equals(null, null));
 	}
 
-	@Test
+//	@Test
 	public void assertEqualsTest() {
 		assertSame(127, 127);
 		assertSame(127, Integer.valueOf(127));
@@ -25,23 +26,23 @@ public class AssertTest {
 		assertNotSame(128, 128);
 		assertNotSame(128, Integer.valueOf(128));
 
-		debug();
+		d();
 	}
 
-	@Test
+//	@Test
 	public void eqListTest() {
 		assertEquals(List.of(3), Arrays.asList(3));
-		debug();
+		d();
 	}
 
-	@Test
+//	@Test
 	public void sameListTest() {
 		var d = List.of(3);
 		assertEquals(d, d);
-		debug();
+		d();
 	}
 
-	void debug() {
+	private void d() {
 	}
 
 }
